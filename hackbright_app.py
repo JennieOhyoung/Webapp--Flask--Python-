ccf2e4d9764268d51189c028db23b6c29fb9a5ac
Amy <amyshire@gmail.com>
2013-10-15 00:37:00 -0700

    Latest version + messing with HTML/CSS

--- hackbright_app.py
+++ hackbright_app.py
@@ -10,12 +10,14 @@ def get_student_by_github(github):
     row = DB.fetchone()
     return row
 
+
 # add a student
 def make_new_student(first_name, last_name, github):
     query = """INSERT into Students values (?, ?, ?)"""
     DB.execute(query, (first_name, last_name, github))
     CONN.commit()
     return "Successfully added student: %s %s" % (first_name, last_name)
+
 
 # query for projects by title
 def get_project_by_title(title):
@@ -27,6 +29,7 @@ def get_project_by_title(title):
         Description: %s
         Maximum grade: %d""" % (row[0], row[1], row[2])
 
+
 # add a project
 def add_project(title, description, max_grade):
     query = """INSERT INTO Projects values (?, ?, ?)"""
@@ -34,16 +37,14 @@ def add_project(title, description, max_grade):
     CONN.commit()
     print "Successfully added Project: %s %s %s" % (title, description, max_grade)
 
+
 # query for a student's grade given a project
 def student_grade_project(project_title):
     query = """SELECT student_github, project_title, grade FROM Grades WHERE project_title = ?"""
     DB.execute(query, (project_title,))
     row = DB.fetchall()
     return row
-    # print """\
-    #     Student Github: %s
-    #     Project Title: %s
-    #     Grade: %d""" % (row[0], row[1], row[2])
+    
 
 # give a grade to a student
 def give_grade_to_student(student_github, project_title, grade):
@@ -52,12 +53,14 @@ def give_grade_to_student(student_github, project_title, grade):
     CONN.commit()
     print "Successfully added: %s, %s, %s to Grades." % (student_github, project_title, grade)
 
+
 # show all the grades for a student
 def show_all_grades(student_github):
     query = """SELECT student_github, project_title, grade FROM Grades WHERE student_github = ?"""
     DB.execute(query, (student_github,))
     rows = DB.fetchall()
     return rows
+
 
 # database connection
 def connect_to_db():
--- webapp.py
+++ webapp.py
@@ -3,50 +3,58 @@ import hackbright_app
 
 app = Flask(__name__)
 
-"""
-@app.route("/")
-def helloworld():
-    return "Hello World!"
-"""
 
 @app.route("/")
 def get_github():
     return render_template("get_github.html")
 
+
 @app.route("/student")
 def get_student():
     hackbright_app.connect_to_db()
+    
     student_github = request.args.get("github")
+    
     row = hackbright_app.get_student_by_github(student_github)
+    
     html = render_template("student_info.html", first_name = row[0], last_name = row[1], github = row[2])
     return html
+
 
 @app.route("/allgrades")
 def get_all_grade():
     hackbright_app.connect_to_db()
-    student_github=request.args.get("github")
+    
+    student_github = request.args.get("github")
+    
     rows = hackbright_app.show_all_grades(student_github)
+    
     html = render_template("student_grades.html", student_github = student_github, grades = rows)
     return html
+
 
 @app.route("/projects")
 def get_student_grade():
     hackbright_app.connect_to_db()
-    project_title=request.args.get("project") 
+    
+    project_title = request.args.get("project") 
+    
     rows = hackbright_app.student_grade_project(project_title)
+    
     html = render_template("project_info.html", project_title = project_title, grades = rows)
     return html
+
 
 @app.route("/newstudent")
 def new_student():
     hackbright_app.connect_to_db()
-
+    
     first_name = request.args.get("first_name")
     last_name = request.args.get("last_name")
     github = request.args.get("github")
-
+    
     hackbright_app.make_new_student(first_name, last_name, github)
-
+    
     html = render_template("new_student.html", first_name = first_name, last_name = last_name, github = github)
     return html
 
@@ -63,6 +71,21 @@ def new_project():
 
     html = render_template("new_project.html", title = title, description = description, max_grade = max_grade)
     return html
+
+
+@app.route("/givegrade")
+def give_grade():
+    hackbright_app.connect_to_db()
+    
+    student_github = request.args.get("github")
+    project_title = request.args.get("title")
+    grade = request.args.get("grade")
+    
+    hackbright_app.give_grade_to_student(student_github, project_title, grade)
+    
+    html = render_template("give_grade.html", student_github = student_github, project_title = project_title, grade = grade)
+    return html
+
 
 if __name__ == "__main__":
     app.run(debug=True)
