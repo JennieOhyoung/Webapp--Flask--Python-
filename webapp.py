cb616d6f211eaa1e4c5730e27ef7310db3ce593e
Hackbright Student <user@chromebox06.(none)>
2013-10-15 18:15:25 -0700

    Updated with redirect

--- webapp.py
+++ webapp.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 import hackbright_app
 
 app = Flask(__name__)
@@ -37,47 +37,40 @@ def get_all_grade():
 @app.route("/projects")
 def get_student_grade():
     hackbright_app.connect_to_db()
-    
     project_title = request.args.get("project") 
-    
     rows = hackbright_app.student_grade_project(project_title)
-    
     html = render_template("project_info.html", project_title = project_title, grades = rows)
     return html
 
 
-@app.route("/newstudent")
+@app.route("/newstudent", methods=['POST'])
 def new_student():
     hackbright_app.connect_to_db()
-    
-    first_name = request.args.get("first_name")
-    last_name = request.args.get("last_name")
-    github = request.args.get("github")
-    
+    first_name = request.form.get("first_name")
+    last_name = request.form.get("last_name")
+    github = request.form.get("github")
     hackbright_app.make_new_student(first_name, last_name, github)
-    
     html = render_template("new_student.html", first_name = first_name, last_name = last_name, github = github)
     return html
 
 
-@app.route("/newproject")
+@app.route("/newproject", methods=['POST'])
 def new_project():
     hackbright_app.connect_to_db()
-
-    title = request.args.get("title")
-    description = request.args.get("description")
-    max_grade = request.args.get("max_grade")
-
+    title = request.form.get("title")
+    description = request.form.get("description")
+    max_grade = request.form.get("max_grade")
     hackbright_app.add_project(title, description, max_grade)
 
-    html = render_template("new_project.html", title = title, description = description, max_grade = max_grade)
-    return html
+    # html = render_template("new_project.html", title = title, description = description, max_grade = max_grade)
+    # return html
+    return redirect("/projects?project=%s" % title)
 
 
 @app.route("/givegrade")
 def give_grade():
     hackbright_app.connect_to_db()
-    
+
     student_github = request.args.get("github")
     project_title = request.args.get("title")
     grade = request.args.get("grade")
